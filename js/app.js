d9f91b22801339e084bf2915e80efcd5629b7adc
Fabián Huertas <miloh9318@hotmail.com>
2025-05-14 19:58:36 -0500

    creando la clase animal

---
+++ js/app.js
@@ -0,0 +1,11 @@
+class Animal{
+    constructor(nombre, peso, edad) {
+        this.nombre = nombre;
+        this.peso = peso;
+        this.edad = edad;
+    }
+
+    informacion(){
+        return `${this.nombre} - ${this.peso} kg - ${this.edad} año(s)`
+    }
+}
